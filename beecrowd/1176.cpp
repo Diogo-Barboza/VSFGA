b0791652c64230c653873b401a2ab698f81f027c
Diogo-Barboza <diogorodriguesbb@gmail.com>
2024-11-14 17:26:10 -0300

    fix: 1176 - accepted

--- beecrowd/1176.cpp
+++ beecrowd/1176.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibb(int n){
-    if(n == 0)
+unsigned long long fib(unsigned long long n){
+    vector<unsigned long long> numbers;
+    numbers.push_back(0);
+    numbers.push_back(1);
+    unsigned long long fib = 0;
+
+    if (n == 0){
         return 0;
+    }
     if(n == 1){
         return 1;
     }
-    return (fibb(n-1) + fibb(n));
+
+    for (int i = 2; i <= n; i++)
+    {
+        fib = numbers[i - 2] + numbers[i - 1];
+        numbers.push_back(fib);
+    }
+
+    return numbers[n];
 }
 
 int main() {
-    // int ent = 0;
+    int tam = 0;
+    int ent = 0;
 
-    // for (int i = 0; i < ent; i++)
-    // {
-        
-    // }
+    cin >> tam;
 
-    cout << fibb(4) << endl;
-    
+    for (int i = 0; i < tam; i++)
+    {
+        cin >> ent;
 
+        cout << "Fib(" << ent << ") = " << fib(ent) << endl;
+    }
 
     return 0;
 }
