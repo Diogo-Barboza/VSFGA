7d233c8d250b22b33f18422cc4e0013a584d39d9
Diogo-Barboza <diogorodriguesbb@gmail.com>
2024-11-24 22:57:46 -0300

    feat:2295 AC

--- beecrowd/2295.cpp
+++ beecrowd/2295.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main(){
-    
+    float palcool = 0, pgasolia = 0, calcool = 0, cgasolina = 0;
 
+    cin >> palcool >> pgasolia >> calcool >> cgasolina;
+
+    if ((palcool/calcool) < (pgasolia/cgasolina)){
+        cout << "A" << endl;
+    }else
+    {
+        cout << "G" << endl;
+    }
+    
     return 0;
 }
