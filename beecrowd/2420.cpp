9204d366f6915c2b3f5e91b96fcac1f8de072e0c
Diogo-Barboza <diogorodriguesbb@gmail.com>
2024-11-24 22:40:04 -0300

    feat:2420 AC

--- beecrowd/2420.cpp
+++ beecrowd/2420.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 
 int main() {
-        
+    int tam = 0, aux = 0;
+    vector<int> sum;
+
+    int soma = 0, pivo = 0, achou = 0;
+
+    cin >> tam;
+    for (int i = 0; i < tam; i++)
+    {
+        cin >> aux;
+        soma += aux;
+        sum.push_back(aux);
+    }
+
+    pivo = soma / 2;
+    soma = 0;
+
+    for (int i = 0; i < sum.size(); i++)
+    {
+        soma += sum[i];
+        if(soma == pivo){
+            achou = (i + 1);
+            break;
+        }
+    }
+    
+    cout << achou << endl;
 
     return 0;
 }
