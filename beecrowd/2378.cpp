e56f3448e5b78eb27b01bf6cd42f28eedb7b6d32
Diogo-Barboza <diogorodriguesbb@gmail.com>
2024-11-25 23:20:10 -0300

    feat:2378 AC

--- beecrowd/2378.cpp
+++ beecrowd/2378.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 int main(){
+    int tam = 0, MAXCAP = 0, saida = 0, entrada = 0;
+    int sumtotal = 0, merda = 0;
+
+
+    cin >> tam >> MAXCAP;
+
+    for (int i = 0; i < tam; i++)
+    {
+        cin >> saida >> entrada;
+        sumtotal += (entrada - saida);
+        if(sumtotal > MAXCAP){
+            merda++;
+        }
+    }
     
+    if(merda > 0){
+        cout << "S" << endl;
+    }else{
+        cout << "N" << endl;
+    }
 
     return 0;
 }
