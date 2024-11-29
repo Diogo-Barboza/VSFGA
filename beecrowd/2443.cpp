584d446caa62a921fcc83199453b0ef64a6b8a5e
Diogo-Barboza <diogorodriguesbb@gmail.com>
2024-11-28 23:14:59 -0300

    feat: 2443 AC

--- beecrowd/2443.cpp
+++ beecrowd/2443.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int num1, int num2){
-    if(num1%num2==0)
-        return num2;
-    else
-        return gcd(num2, num1%num2);
+int gcd(int a, int b){
+    while(b != 0){
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+//Algoritmo do MMC
+int mmc(int a, int b){
+    return a * (b / gcd(a, b));
 }
 
 int main(){
-    cout << gcd(55, 21) << endl;
     int a, b, c, d, mdc1, mdc2 = 0;
     cin >> a >> b >> c >> d;
+
     int frac1, frac2 = 0;
     int cima, baixo = 0;
 
-    if (b % d == 0 || d % b == 0) // 3 3 ou 3 6
-    {
-        if (b == d || b > d)
-        {
-            baixo = b;
-        }else
-        {
-            baixo = d;
-        }
-        
-    }else{
-        baixo = b * d;
-    }
-    
-    cima = ((baixo / b) * a) + ((baixo / d) * c);
 
-    
-    if(gcd(cima, baixo) > 1){
-        cima = (cima / gcd(cima, baixo));
-        baixo = (baixo / gcd(cima, baixo));  
-    }
+    baixo = mmc(b, d);   
+    cima = ((baixo / b) * a) + ((baixo / d) * c); 
 
-    cout << cima << baixo << endl;
+    // cout << "cima: " << cima << endl;
+    // cout << "baixo: " << baixo << endl;
+
+    int mdc = gcd(cima, baixo);
+    //cout << "MDC: " << mdc << endl;
+    
+    cima = (cima / mdc);
+    baixo = (baixo / mdc);  
+
+    cout << cima << " " << baixo << endl;
 
     return 0;
 }
