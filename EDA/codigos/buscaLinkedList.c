a842dd3c9767eb81a2366e32d36990e6c8cd9104
Diogo-Barboza <diogorodriguesbb@gmail.com>
2025-06-07 21:05:08 -0300

    MOJ Busca Lista Encadeada

--- EDA/codigos/buscaLinkedList.c
+++ EDA/codigos/buscaLinkedList.c
@@ -6,64 +6,15 @@ typedef struct celula{
     struct celula *prox;
 }celula;
 
-celula *busca(celula *le, int x){
+celula *busca(celula *le, int x){ //Busca Iterativa
     for (celula *ptr = le; ptr != NULL; ptr = ptr -> prox){
         if(ptr -> dado == x) return ptr;
     }
     return NULL;
 }
 
-celula *busca_rec (celula *le, int x){
+celula *busca_rec (celula *le, int x){ // Busca recursiva
     if ( le == NULL ) return NULL;
     if (le -> dado == x) return le;
     return busca_rec(le -> prox, x);
-}
-
-celula *cria_lista(){
-    celula *le = malloc(sizeof(celula));
-
-    le -> prox = NULL;
-
-    return le;
-}
-
-void insere(celula *le, int x){
-    celula *novo = malloc(sizeof(celula));
-    novo -> dado = x;
-    novo -> prox = le -> prox;
-    le -> prox = novo;
-}
-
-void imprime(celula *le){
-    printf("Lista: -> ");
-    for (celula *ptr = le -> prox; ptr != NULL; ptr = ptr -> prox){
-        printf("%d -> ", ptr -> dado);
-    }
-    printf("NULL\n");
-}
-
-int main(){
-    celula *le = cria_lista();
-
-    insere(le, 1);
-    insere(le, 2);
-    insere(le, 43);
-    insere(le,90);
-    insere(le, 100);
-    insere(le, 110);
-
-    imprime(le);
-
-    celula *buscaNormal = busca(le->prox, 43);
-
-    celula *buscaRecusiva = busca_rec(le->prox, 43);
-
-    if(buscaNormal != NULL){
-        printf("Found the value %d in the list!\n", buscaNormal->dado);
-    } else {
-        printf("Could not find the value %d in the list.\n", 43);
-    }
-
-
-    return 0;
 }
--- EDA/codigos/listaEncadeada.c
+++ EDA/codigos/listaEncadeada.c
@@ -6,7 +6,7 @@ typedef struct celula{
     struct celula *prox;
 }celula;
 
-celula *criar_lista(){
+celula *cria_lista(){
     celula *le = malloc(sizeof(celula));
 
     le -> prox = NULL;
@@ -14,21 +14,27 @@ celula *criar_lista(){
     return le;
 }
 
-celula *busca(celula *le, int x){
-    for(celula *ptr; ptr != NULL; ptr = ptr -> prox){
+celula *busca(celula *le, int x){ //Busca Iterativa
+    for (celula *ptr = le; ptr != NULL; ptr = ptr -> prox){
         if(ptr -> dado == x) return ptr;
-        return NULL;
     }
+    return NULL;
 }
 
-void insere(celula *ptr, int x){
+celula *busca_rec (celula *le, int x){ // Busca recursiva
+    if ( le == NULL ) return NULL;
+    if (le -> dado == x) return le;
+    return busca_rec(le -> prox, x);
+}
+
+void insere(celula *le, int x){
     celula *novo = malloc(sizeof(celula));
     novo -> dado = x;
-    novo -> prox = ptr -> prox;
-    ptr -> prox = novo;
+    novo -> prox = le -> prox;
+    le -> prox = novo;
 }
 
-int remove(celula *ptr){ // celula anterior
+int remove(celula *ptr){ // NAO TESTADA AINDA...
     celula *lixo = ptr -> prox;
     int x = ptr -> dado;
     ptr -> prox = lixo -> prox;
@@ -36,10 +42,36 @@ int remove(celula *ptr){ // celula anterior
     return x;
 }
 
-int main(){
+void imprime(celula *le){
+    printf("Lista: -> ");
+    for (celula *ptr = le -> prox; ptr != NULL; ptr = ptr -> prox){
+        printf("%d -> ", ptr -> dado);
+    }
+    printf("NULL\n");
+}
 
-    
-    
-    
+int main(){
+    celula *le = cria_lista();
+
+    insere(le, 1);
+    insere(le, 2);
+    insere(le, 43);
+    insere(le,90);
+    insere(le, 100);
+    insere(le, 110);
+
+    imprime(le);
+
+    celula *buscaNormal = busca(le->prox, 43);
+
+    celula *buscaRecusiva = busca_rec(le->prox, 43);
+
+    if(buscaNormal != NULL){
+        printf("Found the value %d in the list!\n", buscaNormal->dado);
+    } else {
+        printf("Could not find the value %d in the list.\n", 43);
+    }
+
+
     return 0;
 }
