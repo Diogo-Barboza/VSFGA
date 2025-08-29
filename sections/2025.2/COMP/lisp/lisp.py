db7de7f7c9c2de81cbab3d96a9119d770881b609
Diogo-Barboza <diogorodriguesbb@gmail.com>
2025-08-29 17:45:37 -0300

    Deployed 6b7cfa3 with MkDocs version: 1.6.1

---
+++ sections/2025.2/COMP/lisp/lisp.py
@@ -0,0 +1,110 @@
+# pyproject.toml
+from typing import Any
+from dataclasses import dataclass
+import math
+import operator
+
+type AST = list
+type IR = list | callable | float
+
+
+FUNCTIONS = {
+    "sqrt": math.sqrt, 
+    "cos": math.cos, 
+    "sin": math.sin, 
+    "*": operator.mul,
+    "+": operator.add,
+    "-": operator.sub,
+    "/": operator.truediv,
+    "%": operator.mod,
+}
+
+@dataclass
+class Token:
+    text: str
+    kind: str
+    value: Any = None
+
+def lex(source: str) -> list[Token]:
+    """
+    Realiza a análise léxica do código fonte.
+    """
+    source = source.replace("(", " ( ")
+    source = source.replace(")", " ) ")
+    lexemes = source.split()
+    tokens = []
+    
+    for substr in lexemes:
+        if substr == "(":
+            token = Token(substr, "LPAR")
+        elif substr == ")":
+            token = Token(substr, "RPAR")
+        elif substr in FUNCTIONS:
+            impl = FUNCTIONS[substr]
+            token = Token(substr, "FUNCTION", impl)
+        else:
+            value = float(substr)
+            token = Token(substr, "NUMBER", value)
+        tokens.append(token)
+
+    return tokens
+
+
+def parse(tokens: list[Token]) -> AST:
+    """
+    Realiza a análise sintática do código.
+    """
+    from pprint import pprint
+    pprint(tokens)
+    raise NotImplementedError
+
+
+def analysis(ast: AST) -> IR:
+    """
+    Análise semântica.
+    """ 
+    ir = ast
+    return ir
+
+def emit(ir: IR) -> str:
+    """
+    Emite código
+    """
+    raise NotImplementedError("PARA CASA ;)")
+
+def interpret(ir: IR) -> Any:
+    """
+    Interpreta uma árvore sintática/IR.   
+    """
+    if isinstance(ir, float) or callable(ir):
+        return ir
+    elif isinstance(ir, list):
+        func, *args = map(interpret, ir)
+        return func(*args)
+    else:
+        raise RuntimeError("Erro de tipificacao")
+    
+def run(source: str) -> Any:
+    tokens = lex(source)
+    ast = parse(tokens)
+    ir = analysis(ast)
+    value = interpret(ir)
+    
+    print("=", value)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        with open(filename) as fd:
+            source = fd.read()
+    else:
+        while True:
+            try:
+                source = input("lisp> ")
+            except EOFError:
+                break
+            if not source:
+                break
+            run(source)
